5df907319f185748b75292c4fd410c15dcdf3542
xiaohan1219 <txw1219@163.com>
2012-12-22 01:28:20 +0800

    add fed-standard

---
+++ cnDocs/fed-standard.php
@@ -0,0 +1,141 @@
+<?php $page = "fed-standard" ?>
+<?php $title="Web前端开发规范文档"?>
+<?php $description = "前端规范——前端开发团队遵循和约定的代码书写规范，旨在提高代码的规范性和可维护性。" ?>
+<?php include("./templates/header.php"); ?>
+
+<!-- Masthead
+================================================== -->
+<header class="jumbotron subhead" id="overview">
+  <div class="container">
+    <h1 title="Guide">前端规范文档</h1>
+    <p class="lead">前端开发团队遵循和约定的代码书写规范，旨在提高代码的规范性和可维护性。</p>
+  </div>
+</header>
+
+
+  <div class="container">
+
+    <!-- Docs nav
+    ================================================== -->
+    <div class="row">
+      <div class="span3 bs-docs-sidebar">
+        <ul class="nav nav-list bs-docs-sidenav">
+			<li><a href="#overview"><i class="icon-chevron-right"></i> 概述</a></li>
+			<li><a href="#file-structure"><i class="icon-chevron-right"></i> 文档结构</a></li>
+			<li><a href="#naming"><i class="icon-chevron-right"></i> 命名规范</a></li>
+			<li><a href="#labor"><i class="icon-chevron-right"></i> 分工安排</a></li>
+			<li><a href="#notes"><i class="icon-chevron-right"></i> 注释规范</a></li>
+			<li><a href="#css"><i class="icon-chevron-right"></i> CSS规范</a></li>
+			<li><a href="#html"><i class="icon-chevron-right"></i> HTML规范</a></li>
+			<li><a href="#hack"><i class="icon-chevron-right"></i> Hack规则</a></li>
+			<li><a href="#javascript"><i class="icon-chevron-right"></i> JS规范</a></li>
+			<li><a href="#edm"><i class="icon-chevron-right"></i> EDM制作规范</a></li>
+        </ul>
+      </div>
+      <div class="span9">
+		
+		
+		
+		<!-- Overview
+		================================================== -->
+		<section id="overview">
+			<div class="page-header">
+				<h1>概述</h1>
+			</div>
+			<h3>规范说明</h3>
+			<p>此规范为参考规范，不全是硬性要求，部分硬性约定见下一条书写规范，统一团队编码规范和风格。让所有代码都是有规可循的，并且能够得到沉淀，减少重复劳动。</p>
+		</section>
+		
+		
+		
+	    <!-- File Structure
+		================================================== -->
+		<section id="file-structure">
+			<div class="page-header">
+				<h1>文档结构</h1>
+			</div>
+			<p>文档结构</p>
+		</section>
+		
+		<!-- Naming
+		================================================== -->
+		<section id="naming">
+			<div class="page-header">
+				<h1>命名规范</h1>
+			</div>
+			<p>命名规范</p>
+		</section>
+		
+		<!-- JavaScript
+		================================================== -->
+		<section id="labor">
+			<div class="page-header">
+				<h1>分工安排</h1>
+			</div>
+			<p>分工安排</p>
+		</section>
+		
+		
+		<!-- Notes
+		================================================== -->
+		<section id="notes">
+			<div class="page-header">
+				<h1>注释规范</h1>
+			</div>
+			<p>注释规范</p>
+		</section>
+		
+		<!-- CSS
+		================================================== -->
+		<section id="css">
+			<div class="page-header">
+				<h1>CSS规范</h1>
+			</div>
+			<p>CSS规范</p>
+		</section>
+		
+		<!-- HTML
+		================================================== -->
+		<section id="html">
+			<div class="page-header">
+				<h1>HTML规范</h1>
+			</div>
+			<p>HTML规范</p>
+		</section>
+		
+		
+		<!-- Hack
+		================================================== -->
+		<section id="hack">
+			<div class="page-header">
+				<h1>Hack规则</h1>
+			</div>
+			<p>Hack规则</p>
+		</section>
+		
+		<!-- JavaScript
+		================================================== -->
+		<section id="javascript">
+			<div class="page-header">
+				<h1>JS规范</h1>
+			</div>
+			<p>JS规范</p>
+		</section>
+		
+		<!-- EDM
+		================================================== -->
+		<section id="edm">
+			<div class="page-header">
+				<h1>EDM制作规范</h1>
+			</div>
+			<p>EDM制作规范</p>
+		</section>
+
+      </div>
+    </div>
+
+</div>
+
+
+
+<?php include("common/footer.html");?>
